476e1764c9b526393a42119ec2ff89ffbf176ee7
Shatrov Egor <e.shatrov@bellerage.com>
2021-11-25 18:32:40 +0300

    new navbar

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -1,11 +1,36 @@
 import React from "react";
-import './Navbar.css'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const Navbar = () => {
-    return(
-        <div className='navbar'>
+    const theme = createTheme({
+        palette: {
+          neutral: {
+            main: '#354461',
+            contrastText: '#fff',
+          },
+        },
+      })
 
-        </div>
+    return(
+        <ThemeProvider theme={theme}>
+            <Box sx={{ width:"100vw" }}>
+                <AppBar 
+                    position="static"
+                    color="neutral"
+                    >
+                <Toolbar variant="regular">
+                    <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                    <MenuIcon />
+                    </IconButton>
+                </Toolbar>
+                </AppBar>
+            </Box>
+        </ThemeProvider>
     )
 }
 
